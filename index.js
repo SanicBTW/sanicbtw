c1f6c3216a00ea1fb10f1307247f0cac781d0c4e
SanicBTW <sansunder718jqda@gmail.com>
2022-06-26 22:28:24 +0200

    Add a funny easter egg (geez it took a lot of time lol)

--- index.js
+++ index.js
@@ -32,13 +32,14 @@ styles.setStyle('githubImage', "width: 50px;");
 elements.addImage("./assets/images/icons/spotify.png", "spotifyImage");
 elements.addAnchor("https://open.spotify.com/user/sansunder718?si=bd80fcafede34e0c/", "", "spotifyLink");
 
+//the same with this one lmao
 elements.addImage("./assets/images/icons/osu.png", "osuImage");
 elements.addAnchor("https://osu.ppy.sh/users/18296030", "", "osuLink");
 
 elements.addImage("./assets/images/icons/steam.png", "steamImage");
 elements.addAnchor("https://steamcommunity.com/id/sancodezapato/", "", "steamLink");
 
-styles.setStyle("centerHeader", "font-family: 'Courier New', Courier, monospace; text-align: center;")
+styles.setStyle("centerHeader", "text-align: center;")
 styles.setStyle(["alsoKnownAs", "lol"], "font-family: 'Courier New', Courier, monospace; text-align: left;");
 styles.setStyle(['spotifyImage', "osuImage", "steamImage"], "width: 50px; margin-left: 1rem;");
 
@@ -53,3 +54,77 @@ elements.setAttribute(["githubLink", "spotifyLink", "osuLink", "steamLink"], 'ta
 
 elements.appendTo(["githubLink", "spotifyLink", "osuLink", "steamLink"], "iconsDiv");
 elements.appendTo(["centerHeader", "alsoKnownAs", "lol", "iconsDiv"], "mainDiv");
+
+//for the easter egg
+var easterEggWord = ["f", "n", "f"];
+
+var pressedKeys = [];
+
+elements.onKeydown(function(key) {
+    if(pressedKeys.length < 3)
+    {
+        pressedKeys.push(key.key);
+        var check = checkVal();
+
+        if(check == "nothing")
+        {
+            elements.changeText("centerHeader", "Sanic");
+        }
+        if(check == "second")
+        {
+            elements.changeText("centerHeader", "ayo?");
+        }
+        if(check == "last")
+        {
+            elements.changeText("centerHeader", "hehe you know it huh");
+        }
+        if(check == "everything")
+        {
+            window.location.assign("https://sanicbtw.github.io/FNF-PsychEngine-0.6-HTML5/");
+            console.log("Success easter egg, resetting array");
+            elements.changeText("centerHeader", "Redirecting...");
+            pressedKeys = [];
+        }
+        if(check == false)
+        {
+            elements.changeText("centerHeader", "Sanic");
+            console.log("Failed easter egg, resetting array");
+            pressedKeys = [];
+        }
+    }
+    else
+    {
+        elements.changeText("centerHeader", "Sanic");
+        console.log("Failed easter egg, resetting array");
+        pressedKeys = [];
+    }
+});
+
+function checkVal()
+{
+    if(pressedKeys[0] == easterEggWord[0] &&
+        pressedKeys[1] == easterEggWord[1] &&
+        pressedKeys[2] == easterEggWord[2])
+        {
+            return "everything";
+        }
+    else if(pressedKeys[0] == easterEggWord[0] &&
+        pressedKeys[1] == easterEggWord[1] &&
+        pressedKeys[2] != easterEggWord[2])
+        {
+            return "last";
+        }
+    else if(pressedKeys[0] == easterEggWord[0] &&
+        pressedKeys[1] != easterEggWord[1] &&
+        pressedKeys[2] != easterEggWord[2])
+        {
+            return "second";
+        }
+    else if(pressedKeys[0] != easterEggWord[0] &&
+        pressedKeys[1] != easterEggWord[1] &&
+        pressedKeys[2] != easterEggWord[2])
+        {
+            return "nothing";
+        }
+    return false;
+}
