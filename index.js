beda53f90b342748a03bce935af09cb58290b965
SanicBTW <sansunder718jqda@gmail.com>
2022-06-26 02:52:57 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+var elements = new ElementsHandler();
+var styles = new StylesHandler();
+
+styles.setFavicon('./assets/images/favicon.jpg');
+
+styles.setBodyStyle('background: linear-gradient(0deg, rgba(40,40,40,1) 0%, rgba(20,20,20,1) 35%, rgba(0,0,0,1) 100%);');
+styles.setBodyStyle("font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; color: white;");
+
+if(platform == "Desktop")
+{
+    styles.setClassStyle("outer", `width: 100%; height: 98vh; display: flex; justify-content: center; align-items: center;`);
+}
+else
+{
+    styles.setClassStyle("outer", `width: 100%; height: 90vh; display: flex; justify-content: center; align-items: center; `);
+}
+
+elements.addDiv("first");
+elements.addDiv("mainDiv");
+elements.addDiv("iconsDiv");
+elements.setClass("first", "outer");
+
+elements.addHeader("Sanic", "h1", "centerHeader");
+elements.addParagraph("aka 'SanicBTW', 'Sanco', 'Saniczoned'", "alsoKnownAs");
+elements.addParagraph("and idk what else", "lol");
+
+elements.addImage("./assets/images/icons/icons8-github-96.png", "githubImage");
+elements.addAnchor("https://github.com/SanicBTW/", "", "githubLink");
+styles.setStyle('githubImage', "width: 50px;");
+
+//why did i add thsi one lol
+elements.addImage("./assets/images/icons/icons8-spotify-96.png", "spotifyImage");
+elements.addAnchor("https://open.spotify.com/user/sansunder718?si=bd80fcafede34e0c/", "", "spotifyLink");
+styles.setStyle('spotifyImage', "width: 50px; margin-left: 1rem;");
+
+styles.setStyle("centerHeader", "font-family: 'Courier New', Courier, monospace; text-align: center;")
+styles.setStyle(["alsoKnownAs", "lol"], "font-family: 'Courier New', Courier, monospace; text-align: left;");
+
+elements.appendTo("mainDiv", "first");
+
+elements.appendTo("githubImage", "githubLink");
+elements.appendTo("spotifyImage", "spotifyLink");
+
+elements.appendTo(["githubLink", "spotifyLink"], "iconsDiv");
+elements.appendTo(["centerHeader", "alsoKnownAs", "lol", "iconsDiv"], "mainDiv");
